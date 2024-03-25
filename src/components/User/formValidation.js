4e5211938cbe357217e51bc01b000834cf0353e6
PeterHassaballah <peter.hassaballah@gmail.com>
2024-03-25 19:18:36 +0200

    [feat] change form validation

--- src/components/User/formValidation.js
+++ src/components/User/formValidation.js
@@ -3,5 +3,5 @@ import * as Yup from 'yup'
 export const formSchema = Yup.object({
     name: Yup.string().min(3).required('Please enter your name.'),
     email: Yup.string().email().required('Please enter valid email.'),
-    role: Yup.string().required('Please enter role.')
+    age: Yup.number().required('Please enter Age.')
 })
