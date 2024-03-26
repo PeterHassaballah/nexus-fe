ac7e4d027fc91397e1280e4a12a5c637f3f7ffaa
PeterHassaballah <peter.hassaballah@gmail.com>
2024-03-26 15:17:44 +0200

    [fix] netlify redirecct

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/my-app/', // Add the base option here
   build: {
     manifest: true,
     rollupOptions: {
